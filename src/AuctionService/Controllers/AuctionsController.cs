6eb3babfa0aa3717fd7f033d96e5665c003c9686
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-26 02:36:01 +0300

    added AuctionsController
    added GetAllAuctions, GetAuctionById and CreateAuction actions

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,52 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions.Include(x => x.Item).OrderBy(x => x.Item.Make).ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null) return NotFound();
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+    {
+        var auction = _mapper.Map<Auction>(createAuctionDto);
+        //TODO: ADD CURRENT USER AS SELLER
+        auction.Seller = "test";
+        _context.Auctions.Add(auction);
+        //SAVE CHANGES BAŞARI ILE KAYIT ETTIGI KAYITLARIN SAYISINI INT OLARAK DONER
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest("Could not save changes to the database");
+        return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
+
+    }
+}
