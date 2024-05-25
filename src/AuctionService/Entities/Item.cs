9088d1fbb74e390029ddcce4d33e8b4231dbe505
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-25 23:14:12 +0300

    initial Commit

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace AuctionService.Entities;
+
+public class Item
+{
+public Guid Id { get; set; }
+public string Make { get; set; }
+public string Model { get; set; }
+public int Year { get; set; }
+public string Color { get; set; }
+public int Mileage { get; set; }
+public string ImageUrl { get; set; }
+
+// Navigation Properties
+}
