dcbe16cbcd551d08ac16754b1e89a44b20f87eae
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-26 22:49:15 +0300

    updated AuctionController

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -49,4 +49,43 @@ public class AuctionsController : ControllerBase
         return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
 
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null) return NotFound();
+        //TODO : seller == username kontrolü yapılacak
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _context.SaveChangesAsync() > 0;
+        if (result)
+        {
+            return Ok();
+        }
+        else
+        {
+            return BadRequest("Problem while saving changes!");
+        }
+
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+        if (auction == null) return NotFound();
+
+        //TODO : seller == username kontrolü yapılacak
+
+        _context.Remove(auction);
+        var result = await _context.SaveChangesAsync() > 0;
+        if (result) return Ok();
+        return BadRequest("Error while deleting auction");
+    }
+
 }
--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -4,8 +4,8 @@ public class UpdateAuctionDto
 {
     public string Make { get; set; }
     public string Model { get; set; }
-    public int Year { get; set; }
+    public int? Year { get; set; }
     public string Color { get; set; }
-    public int Mileage { get; set; }
+    public int? Mileage { get; set; }
 
 }
