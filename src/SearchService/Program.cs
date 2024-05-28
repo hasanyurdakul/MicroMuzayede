a4d96babe50b4b889683d617d59e993fc1cfa538
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-28 14:12:38 +0300

    added SearchService

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,30 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (System.Exception ex)
+{
+
+    System.Console.WriteLine(ex); ;
+}
+
+app.Run();
