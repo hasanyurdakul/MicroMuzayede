986bc18ee5e15805c598b9830d56df395adb6d4d
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-01 02:06:32 +0300

    Create AuctionCreatedConsumer.cs

---
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+{
+    private readonly IMapper _mapper;
+
+    public AuctionCreatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        Console.WriteLine("#### Consuming AuctionCreated Event ####\n" + context.Message.Id);
+        var item = _mapper.Map<Item>(context.Message);
+        await item.SaveAsync();
+    }
+}
