d267f896ed26e5d5af45cb82d08553eb95c12b20
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-01 02:53:57 +0300

    updated Program.cs
    massTransit endpoint formatting in searchService

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddMassTransit(x =>
         cfg.ConfigureEndpoints(context);
     });
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("SearchService"));
 });
 var app = builder.Build();
 
