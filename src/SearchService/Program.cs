9e7879d63f2d07925940d29cc56aa0a0b394c29f
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-01 01:20:54 +0300

    Update Program.cs
    registered AutoMapper

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 builder.Services.AddMassTransit(x =>
 {
