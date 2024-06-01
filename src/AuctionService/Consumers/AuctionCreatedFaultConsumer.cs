6732c762fdb2d0c946d06b0d20afc50c936cbb2b
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-01 06:05:24 +0300

    added AuctionCreatedFaultConsumer
    when consuming

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+//AUCTIONCREATED EVENTI, SEARCHSERVICE ICINDE BIR HATA FIRLATTIGINDA, BU HATA RABBITMQ'DAKI FAULT QUEUE'YA GONDERILIR. BU CONSUMER, BU QUEUE'DAN GELEN HATALARI YAKALAR VE HATALARI KONTROL EDER. EGER HATA TURU ARGUMENTEXCEPTION ISE, MODEL DEGERINI "BAR" OLARAK DEGISTIRIR VE YENIDEN PUBLISH EDER. EGER HATA TURU ARGUMENTEXCEPTION DEGILSE, IGNORELANIR
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        var exception = context.Message.Exceptions.First();
+        Console.WriteLine("#### Consuming Faulty Creation ####\n" + exception.Message);
+        if (exception.ExceptionType == typeof(ArgumentException).Name)
+        {
+            context.Message.Message.Model = "Bar";
+            await context.Publish(context.Message.Message);
+        }
+        else
+        {
+            Console.WriteLine("Not an ArgumentException, ignoring...");
+        }
+    }
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,3 +1,4 @@
+using AuctionService;
 using AuctionService.Data;
 using AuctionService.RequestHelpers;
 using MassTransit;
@@ -13,16 +14,20 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit(x =>
 {
-    x.UsingRabbitMq((context, cfg) =>
-    {
-        cfg.ConfigureEndpoints(context);
-    });
     x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
     {
         o.QueryDelay = TimeSpan.FromSeconds(10);
         o.UseBusOutbox();
         o.UsePostgres();
     });
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+
+
 });
 
 var app = builder.Build();
