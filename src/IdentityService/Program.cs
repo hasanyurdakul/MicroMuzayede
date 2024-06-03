ead87acdfbd3940653b276ab4be258b77d0f4b96
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-03 17:44:53 +0300

    update HostingExtensions, SeedData, Program.cs

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -20,15 +20,7 @@ try
         .ConfigureServices()
         .ConfigurePipeline();
 
-    // this seeding is only for the template to bootstrap the DB and users.
-    // in production you will likely want a different approach.
-    if (args.Contains("/seed"))
-    {
-        Log.Information("Seeding database...");
-        SeedData.EnsureSeedData(app);
-        Log.Information("Done seeding database. Exiting.");
-        return;
-    }
+    SeedData.EnsureSeedData(app);
 
     app.Run();
 }
