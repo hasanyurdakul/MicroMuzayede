c8aec632b59eae007639cac674e29d79ee4e0223
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-04 16:57:31 +0300

    Create BidPlacedConsumer.cs

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("#### Consuming Bid Placed ####\n" + context.Message.AuctionId);
+
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        if (auction.CurrentHighBid == null
+            || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
