e2bd475e3287290741488581cbcce62c3ea639d4
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-04 16:48:45 +0300

    Update AuctionFinishedConsumer.cs

--- src/AuctionService/Consumers/AuctionFinishedConsumer.cs
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -15,6 +15,7 @@ public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
     }
     public async Task Consume(ConsumeContext<AuctionFinished> context)
     {
+        Console.WriteLine("#### Consuming Auction Finished ####\n" + context.Message.AuctionId);
         var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
         if (auction == null)
         {
