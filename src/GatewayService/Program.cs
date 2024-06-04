85467c61cd6224f86ba400e78b5d4c9f2e3d83e4
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-04 14:49:14 +0300

    Update Program.cs

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -6,19 +6,20 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.Authority = builder.Configuration["IdentityServer:Authority"];
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters.ValidateAudience = false;
-    options.TokenValidationParameters.NameClaimType = "username";
-});
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServer:Authority"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
 
 var app = builder.Build();
+
+app.MapReverseProxy();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapReverseProxy();
 
 app.Run();
