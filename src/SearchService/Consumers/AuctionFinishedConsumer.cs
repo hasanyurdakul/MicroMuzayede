36926204119bafa9cab3a7f877a138be41673a3c
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-04 17:10:53 +0300

    Create AuctionFinishedConsumer.cs

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("#### Consuming Auction Finished Event ####\n" + context.Message.AuctionId);
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = (int)context.Message.Amount;
+        }
+        auction.Status = "Finished";
+        await auction.SaveAsync();
+    }
+}
