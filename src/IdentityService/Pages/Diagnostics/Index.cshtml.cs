34e029c967c909800792b69164a69b95f8b88eb6
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-05 17:08:10 +0300

    Update Index.cshtml.cs
    added the docker's remote ip address

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -16,8 +16,8 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new List<string?> { "127.0.0.1", "::1" };
-        if(HttpContext.Connection.LocalIpAddress != null)
+        var localAddresses = new List<string?> { "::ffff:172.25.0.1", "::ffff:172.24.0.1", "127.0.0.1", "::1" };
+        if (HttpContext.Connection.LocalIpAddress != null)
         {
             localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
         }
@@ -28,7 +28,7 @@ public class Index : PageModel
         }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
-            
+
         return Page();
     }
 }
