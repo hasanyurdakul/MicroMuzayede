0f1e29278f7f7839c8f31f9b1fde159e25eafae5
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-05 14:43:14 +0300

    Update Program.cs

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -27,6 +27,11 @@ builder.Services.AddMassTransit(x =>
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         cfg.ConfigureEndpoints(context);
     });
 });
