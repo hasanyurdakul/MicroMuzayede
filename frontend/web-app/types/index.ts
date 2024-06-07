3eb79f52a08ed0b6f00c4d054277e7f08c822bd3
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-07 13:59:49 +0300

    Created Types

---
+++ frontend/web-app/types/index.ts
@@ -0,0 +1,24 @@
+export interface PagedResult<T> {
+  results: T[];
+  pageCount: number;
+  totalCount: number;
+}
+
+export type Auction = {
+  reservePrice: number;
+  seller: string;
+  winner?: any;
+  soldAmount: number;
+  currentHighBid: number;
+  createdAt: string;
+  updatedAt: string;
+  auctionEnd: string;
+  status: string;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+  mileage: number;
+  imageUrl: string;
+  id: string;
+};
