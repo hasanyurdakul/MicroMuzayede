ab893d5a94f38d9e62fc77772a21f8e94e40b10d
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-07 04:40:06 +0300

    Create AuctionCard.tsx

---
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import CountdownTimer from "./CountdownTimer";
+
+type Props = {
+  auction: any;
+};
+
+export default function AuctionCard({ auction }: Props) {
+  return (
+    <a href="#">
+      <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
+        <div>
+          <Image
+            src={auction.imageUrl}
+            alt="image"
+            fill
+            priority
+            className="object-cover h-auto"
+            sizes="(max-width: 1200px) 50vw, (max-width: 768px) 100vw, 25vw"
+          />
+          <div className="absolute bottom-2 left-2">
+            <CountdownTimer auctionEnd={auction.auctionEnd} />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-4">
+        <h3 className="text-gray-700">
+          {auction.make} {auction.model}
+        </h3>
+        <p className="font-semibold text-sm">{auction.year}</p>
+      </div>
+    </a>
+  );
+}
