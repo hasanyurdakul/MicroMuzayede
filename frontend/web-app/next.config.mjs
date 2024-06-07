e9e2c05e8fabbf9e11b277fbf47c41f75037377e
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-07 04:39:55 +0300

    Update next.config.mjs

--- frontend/web-app/next.config.mjs
+++ frontend/web-app/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["cdn.pixabay.com", "images.unsplash.com"],
+  },
+};
 
 export default nextConfig;
