08f80871e70740f7a4c5ccb35d2f405c0bb36615
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-14 14:25:16 +0300

    update auctionAction.ts for testing update api on client

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -8,3 +8,24 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
   }
   return result.json();
 }
+
+export async function UpdateAuctionTest() {
+  const data = {
+    mileage: Math.floor(Math.random() * 100000) + 1,
+  };
+
+  const res = await fetch(
+    "http://localhost:6001/auctions/c8c3ec17-01bf-49db-82aa-1ef80b833a9f",
+    {
+      method: "PUT",
+      headers: {},
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!res.ok) {
+    return { status: res.status, message: res.statusText };
+  }
+
+  return res.statusText;
+}
