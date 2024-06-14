644592518fa505979b02d7ffecc284927649691b
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-14 03:05:45 +0300

    Update Logo.tsx

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { useParamsStore } from "@/hooks/useParamsStore";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { IoCarSport } from "react-icons/io5";
 
 export default function Logo() {
+  const router = useRouter();
+  const pathName = usePathname();
   const reset = useParamsStore((state) => state.reset);
+  function doReset() {
+    if (pathName !== "/") {
+      router.push("/");
+      reset();
+    }
+  }
   return (
     <div
-      onClick={reset}
+      onClick={doReset}
       className="flex cursor-pointer items-center gap-2 text-3xl font-semibold text-red-500"
     >
       <IoCarSport size={34} />
