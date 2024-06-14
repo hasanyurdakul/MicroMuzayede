a9258cc7f7e342389b5b59be165980941b067348
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-14 14:30:45 +0300

    Create AuthTest.tsx

---
+++ frontend/web-app/app/session/AuthTest.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { useState } from "react";
+import { UpdateAuctionTest } from "../actions/auctionActions";
+import { Button } from "flowbite-react";
+
+export default function AuthTest() {
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState<any>();
+
+  function doUpdate() {
+    setResult(undefined);
+    setLoading(true);
+    UpdateAuctionTest()
+      .then((res) => setResult(res))
+      .finally(() => setLoading(false));
+  }
+  return (
+    <div className="flex items-center gap-4">
+      <Button outline isProcessing={loading} onClick={doUpdate}>
+        Test Auth
+      </Button>
+      <div> {JSON.stringify(result, null, 2)} </div>
+    </div>
+  );
+}
