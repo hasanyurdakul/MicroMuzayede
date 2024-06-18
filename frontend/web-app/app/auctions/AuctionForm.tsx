c3aa7dfb690d537ae4484641c50816af486490e4
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-18 13:22:56 +0300

    added reactdatepicker and reacthookform

---
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AuctionForm() {
+  return <div>AuctionForm Goes Here</div>;
+}
--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,5 +1,15 @@
+import Heading from "@/app/components/Heading";
 import React from "react";
+import AuctionForm from "../AuctionForm";
 
 export default function Create() {
-  return <div>Create Auction</div>;
+  return (
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Sell your car!"
+        subtitle="Please enter the details of your car"
+      />
+      <AuctionForm />
+    </div>
+  );
 }
