436ba8b5bec4eaf0bb8584a980b3967f396ab244
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-18 12:59:03 +0300

    myauctions and auctionswon filters

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -19,6 +19,8 @@ export default function Listings() {
       searchTerm: state.searchTerm,
       orderBy: state.orderBy,
       filterBy: state.filterBy,
+      seller: state.seller,
+      winner: state.winner,
     }),
     shallow
   );
--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,24 +1,41 @@
 "use client";
+import { useParamsStore } from "@/hooks/useParamsStore";
 import { Button, Dropdown } from "flowbite-react";
 import { User } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai";
 import { HiCog, HiUser } from "react-icons/hi";
 
 type Props = {
-  user: Partial<User>;
+  user: User;
 };
 
 export default function UserActions({ user }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore((state) => state.setParams);
+  function setWinner() {
+    setParams({ winner: user.username, seller: undefined });
+    if (pathname !== "/") {
+      router.push("/");
+    }
+  }
+  function setSeller() {
+    setParams({ winner: undefined, seller: user.username });
+    if (pathname !== "/") {
+      router.push("/");
+    }
+  }
   return (
     <Dropdown label={`Welcome ${user.name}`} inline>
-      <Dropdown.Item icon={HiUser}>
-        <Link href="/">My Auctions</Link>
+      <Dropdown.Item icon={HiUser} onClick={setSeller}>
+        My Auctions
       </Dropdown.Item>
-      <Dropdown.Item icon={AiFillTrophy}>
-        <Link href="/">Auctions Won</Link>
+      <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+        Auctions Won
       </Dropdown.Item>
       <Dropdown.Item icon={AiFillCar}>
         <Link href="/auctions/create">Sell My Car</Link>
