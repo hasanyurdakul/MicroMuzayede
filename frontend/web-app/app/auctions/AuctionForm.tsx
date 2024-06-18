eb30089a1aaebd6133905af61804c19534c43ed8
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-18 13:39:17 +0300

    Created the temporary form

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,5 +1,52 @@
+"use client";
+import { Button, TextInput } from "flowbite-react";
 import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 export default function AuctionForm() {
-  return <div>AuctionForm Goes Here</div>;
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    formState: { isSubmitting, isValid, isDirty, errors },
+  } = useForm();
+
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
+
+  return (
+    <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3 block">
+        <TextInput
+          {...register("make", { required: "Make is required!" })}
+          placeholder="Make"
+          color={errors?.make && "failure"}
+          helperText={errors.make?.message as string}
+        />
+      </div>
+      <div className="mb-3 block">
+        <TextInput
+          {...register("model", { required: "Model is required!" })}
+          placeholder="Model"
+          color={errors?.model && "failure"}
+          helperText={errors.model?.message as string}
+        />
+      </div>
+      <div className="flex justify-between">
+        <Button outline color={"gray"}>
+          Cancel
+        </Button>
+        <Button
+          outline
+          color={"success"}
+          isProcessing={isSubmitting}
+          //   disabled={!isValid}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
 }
