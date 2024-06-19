3e53f67dfd303914bd5d3ee111b30a939c3e9b16
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-19 22:47:29 +0300

    Create BidsController.cs

---
+++ src/BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BidsController : ControllerBase
+{
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount)
+    {
+        var auction = await DB.Find<Auction>().OneAsync(auctionId);
+        if (auction == null)
+        {
+            //TODO Check with auction service if that has auction
+            return NotFound();
+        }
+
+        if (auction.Seller == User.Identity.Name)
+        {
+            return BadRequest("Cannot bid on your own auction");
+        }
+
+        var bid = new Bid
+        {
+            AuctionId = auctionId,
+            Bidder = User.Identity.Name,
+            Amount = amount,
+        };
+
+        if (auction.AuctionEnd < DateTime.UtcNow)
+        {
+            bid.BidStatus = BidStatus.Finished;
+        }
+        else
+        {
+            var highBid = await DB.Find<Bid>()
+                            .Match(a => a.AuctionId == auctionId)
+                            .Sort(b => b.Descending(x => x.Amount))
+                            .ExecuteFirstAsync();
+
+            if (highBid != null && amount > highBid.Amount || highBid == null)
+            {
+                bid.BidStatus = amount > auction.ReservePrice ? BidStatus.Accepted : BidStatus.AcceptedBelowReserve;
+            }
+            if (highBid != null && amount <= highBid.Amount)
+            {
+                bid.BidStatus = BidStatus.TooLow;
+            }
+        }
+
+        await DB.SaveAsync(bid);
+        return Ok();
+
+    }
+
+}
