5e0aa46dfa4b63f3b607a28b42b99c68ec50982e
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-19 14:55:39 +0300

    Update auctionActions.ts

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,15 +1,17 @@
 "use server";
+import { fetchWrapper } from "@/lib/fetchWrapper";
 import { Auction, PagedResult } from "@/types";
 import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
 import { cookies, headers } from "next/headers";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
-  const result = await fetch(`http://localhost:6001/search${query}`);
-  if (!result.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return result.json();
+  // const result = await fetch(`http://localhost:6001/search${query}`);
+  // if (!result.ok) {
+  //   throw new Error("Failed to fetch data");
+  // }
+  // return result.json();
+  return await fetchWrapper.get(`search${query}`);
 }
 
 export async function UpdateAuctionTest() {
