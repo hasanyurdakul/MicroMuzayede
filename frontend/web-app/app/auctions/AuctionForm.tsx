985a07ae582cf76826bde9bce55690290fffedb5
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-19 04:23:33 +0300

    Update AuctionForm.tsx

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -2,10 +2,11 @@
 import { Button, TextInput } from "flowbite-react";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 export default function AuctionForm() {
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
@@ -17,22 +18,19 @@ export default function AuctionForm() {
 
   return (
     <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("make", { required: "Make is required!" })}
-          placeholder="Make"
-          color={errors?.make && "failure"}
-          helperText={errors.make?.message as string}
-        />
-      </div>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("model", { required: "Model is required!" })}
-          placeholder="Model"
-          color={errors?.model && "failure"}
-          helperText={errors.model?.message as string}
-        />
-      </div>
+      <Input
+        label={"Make"}
+        name={"make"}
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label={"Model"}
+        name={"model"}
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+
       <div className="flex justify-between">
         <Button outline color={"gray"}>
           Cancel
