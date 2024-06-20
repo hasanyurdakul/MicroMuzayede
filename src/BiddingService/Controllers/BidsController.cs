a13c38c68c69b75421f716a256b9702f425f1f9d
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-20 16:43:55 +0300

    Update BidsController.cs

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.CodeDom.Compiler;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
@@ -9,9 +10,15 @@ namespace BiddingService;
 [Route("api/[controller]")]
 public class BidsController : ControllerBase
 {
+    private readonly IMapper _mapper;
+
+    public BidsController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
     [Authorize]
     [HttpPost]
-    public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount)
+    public async Task<ActionResult<BidDto>> PlaceBid(string auctionId, int amount)
     {
         var auction = await DB.Find<Auction>().OneAsync(auctionId);
         if (auction == null)
@@ -54,20 +61,20 @@ public class BidsController : ControllerBase
         }
 
         await DB.SaveAsync(bid);
-        return bid;
+        return Ok(_mapper.Map<BidDto>(bid));
 
     }
 
 
 
     [HttpGet("{auctionId}")]
-    public async Task<ActionResult<List<Bid>>> GetBidsForAuction(string auctionId)
+    public async Task<ActionResult<List<BidDto>>> GetBidsForAuction(string auctionId)
     {
         var bids = await DB.Find<Bid>()
             .Match(a => a.AuctionId == auctionId)
             .Sort(b => b.Descending(x => x.BidTime))
             .ExecuteAsync();
-        return bids;
+        return bids.Select(_mapper.Map<BidDto>).ToList();
     }
 
 }
