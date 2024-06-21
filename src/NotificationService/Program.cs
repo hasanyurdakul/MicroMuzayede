8fbae32dce862349935344a333362ae0454b34da
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-21 14:21:25 +0300

    Create NotificationService

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,23 @@
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+
+var app = builder.Build();
+
+
+app.Run();
