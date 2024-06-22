33c7b70a32a3bd67767e79a7a45ac0d6951a1954
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-22 22:47:24 +0300

    Create AuctionCreatedToast

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -3,24 +3,26 @@ import "./globals.css";
 import Navbar from "./nav/Navbar";
 import ToasterProvider from "./providers/ToasterProvider";
 import SignalRProvider from "./providers/SignalRProvider";
+import { getCurrentUser } from "./actions/authActions";
 
 export const metadata: Metadata = {
   title: "Micro Muzayede",
   description: "Microservices PoC Client App",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getCurrentUser();
   return (
     <html lang="en">
       <body>
         <ToasterProvider />
         <Navbar />
         <main className="container-fluid max-w-[1200px] mx-auto px-5 pt-10">
-          <SignalRProvider>{children}</SignalRProvider>
+          <SignalRProvider user={user}>{children}</SignalRProvider>
         </main>
       </body>
     </html>
--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { useAuctionStore } from "@/hooks/useAuctionStore";
 import { useBidStore } from "@/hooks/useBidStore";
-import { Bid } from "@/types";
+import { Auction, Bid } from "@/types";
 import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { User } from "next-auth";
 import React, { ReactNode, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import AuctionCreatedToast from "../components/AuctionCreatedToast";
 
 type Props = {
   children: ReactNode;
+  user: User | null;
 };
 
-export default function SignalRProvider({ children }: Props) {
+export default function SignalRProvider({ children, user }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>();
   const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
   const addBid = useBidStore((state) => state.addBid);
@@ -35,6 +39,14 @@ export default function SignalRProvider({ children }: Props) {
               setCurrentPrice(bid.auctionId, bid.amount);
             }
             addBid(bid);
+          });
+
+          connection.on("AuctionCreated", (auction: Auction) => {
+            if (user?.username !== auction.seller) {
+              return toast(<AuctionCreatedToast auction={auction} />, {
+                duration: 10000,
+              });
+            }
           });
         })
         .catch((error) => {
