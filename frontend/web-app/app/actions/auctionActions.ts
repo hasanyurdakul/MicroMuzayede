b914a51ac451851a8291e6b9b0c5672e2bec1a0f
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-22 14:41:52 +0300

    Create placeBidForAuction

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -60,3 +60,10 @@ export async function deleteAuction(id: string) {
 export async function getBidsForAuction(id: string): Promise<Bid[]> {
   return await fetchWrapper.get(`bids/${id}`);
 }
+
+export async function placeBidForAuction(auctionId: string, amount: number) {
+  return await fetchWrapper.post(
+    `bids?auctionId=${auctionId}&amount=${amount}`,
+    {}
+  );
+}
