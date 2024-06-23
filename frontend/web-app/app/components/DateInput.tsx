634fd25d3ea60fc8759a27deead33939d27f358f
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-23 20:20:58 +0300

    Revert "fix ts linter"
    This reverts commit 1c8c90a1d856d768b6d9c874ae3227bff9ff5dd5.

--- frontend/web-app/app/components/DateInput.tsx
+++ frontend/web-app/app/components/DateInput.tsx
@@ -21,19 +21,17 @@ export default function DateInput(props: Props) {
         onChange={(value) => field.onChange(value)}
         selected={field.value}
         placeholderText={props.label}
-        className={`
-                rounded-lg w-[100%] flex flex-col
-                ${
-                  fieldState.error
-                    ? "bg-red-50 border-red-500 text-red-900"
-                    : !fieldState.invalid && fieldState.isDirty
-                    ? "bg-green-50 border-green-500 text-green-900"
-                    : ""
-                }
-            `}
+        className={`rounded-lg w-[100%] flex flex-col
+            ${
+              fieldState.error
+                ? "bg-red-50 border-red-500 text-red-900"
+                : !fieldState.invalid && fieldState.isDirty
+                ? "bg-green-50 border-green-500 text-green-900"
+                : ""
+            }`}
       />
       {fieldState.error && (
-        <div className="text-red-500 text-sm">{fieldState.error.message}</div>
+        <div className="text-red-500 text-sm">{fieldState.error?.message}</div>
       )}
     </div>
   );
